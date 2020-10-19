925126bd78fa0c765553174d1d96bc912999236a
Jelmer Veen <j.p.veen@rug.nl>
2020-10-19 11:34:48 +0200

    added very basic theme chooser PoC

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,37 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/filters">Filters</router-link>
+      <select v-model="selectedTheme" style="margin-left: 1rem">
+        <option :key="theme" v-for="theme in themes" :value="theme">{{ theme }}</option>
+      </select>
+      <button @click="changeTheme()">Save theme</button>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      selectedTheme: 'white.css',
+      themes: ['white.css', 'red.css', 'blue.css'],
+    }
+  },
+  methods: {
+    changeTheme() {
+      const theme = document.createElement('link')
+      theme.className = 'theme-switcher'
+      theme.rel = 'stylesheet'
+      theme.href = `/css/${this.selectedTheme}`
+      document.getElementsByTagName('head')[0].append(theme)
+      document.getElementsByClassName('theme-switcher')[0].remove();
+    },
+  },
+}
+</script>
+
 
 <style lang="scss">
 #app {
